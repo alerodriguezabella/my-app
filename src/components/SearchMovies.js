bb9c7b406986485ee1430d7cdd14cc703dc66eb5
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-07-23 16:31:52 +0200

    Update index file, add SearchMovies component

---
+++ src/components/SearchMovies.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SearchMovies(){
+  return(
+    <form className="form">
+      <label htmlFor="query" className="label">Movie name</label>
+      <input className="input" type='text' name="query" placeholder="I.e. The Ugly Truth"></input>
+      <button className="button" type="submit">Search</button>
+    </form>
+  )
+}
