d0ef3701a6b6c530899a2bca444ef9f23f959e1e
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-07-24 12:16:29 +0200

    Convert Main from class to functional component, solve console errors (ReactDOM with React -v 18)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import SearchMovies from './components/SearchMovies/SearchMovies';
 
-class Main extends React.Component {
-  render() {
-    return (
-      <div className='container'>
-        <h1 className='title'>React Movie Search</h1>
-        <SearchMovies></SearchMovies>
-      </div>
-    );
-  }
-}
+function Main() {
+  return (
+    <div className='container'>
+      <h1 className='title'>React Movie Search</h1>
+      <SearchMovies></SearchMovies>
+    </div>
+  );
+} 
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+export default Main;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Main/>
+);
